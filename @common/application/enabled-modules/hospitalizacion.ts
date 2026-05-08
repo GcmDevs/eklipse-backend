e0e25665537daa6a7b704b0455eeaa185401865f
ejdao <ejdao2022@gmail.com>
2026-05-08 11:24:59 -0500

    :sparkles: Logica para permisos agregada

---
+++ @common/application/enabled-modules/hospitalizacion.ts
@@ -0,0 +1,6 @@
+import { HPN_AUTHORITIES } from '../../../hospitalizacion/authorities';
+
+export function enabledModules_HOSPITALIZACION(authorities: string[]) {
+  let response: string[] = [];
+  return response;
+}
--- @common/application/enabled-modules/index.ts
+++ @common/application/enabled-modules/index.ts
@@ -1,10 +1,12 @@
-import { ADMIN_AUTHORITY } from '@common/application/constants';
+import { ADMIN_AUTHORITY } from '../constants';
 import { enabledModules_GENERAL } from './general';
+import { enabledModules_HOSPITALIZACION } from './hospitalizacion';
 
 
 export function enabledModules(authorities: string[]) {
   let response: string[] = [];
   if (authorities.includes(ADMIN_AUTHORITY)) response.push('admin');
   response.push(...enabledModules_GENERAL(authorities));
+  response.push(...enabledModules_HOSPITALIZACION(authorities));
   return response;
 }
