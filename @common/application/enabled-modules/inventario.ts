0cef4f992bfdb8eb29a76c34b29b4b7fb5990356
ejdao <ejdao2022@gmail.com>
2026-07-23 09:12:45 -0500

    :sparkles: Permisos de farmacia agregados

--- @common/application/enabled-modules/inventario.ts
+++ @common/application/enabled-modules/inventario.ts
@@ -3,7 +3,11 @@ import { INN_AUTHORITIES } from "../../../inventario/authorities";
 export function enabledModules_INVENTARIO(authorities: string[]) {
   let response: string[] = [];
   if (authorities.includes(INN_AUTHORITIES.CODE)) response.push('inn');
+  // SUMINISTROS_PACIENTE
   if (authorities.includes(INN_AUTHORITIES.SUMINISTROS_PACIENTE.CODE)) response.push('inn-sumpac');
   if (authorities.includes(INN_AUTHORITIES.SUMINISTROS_PACIENTE.GESTIONAR)) response.push('inn-sumpac-modificar-recibidos','inn-sumpac-pendientes-by-subgrupo','inn-sumpac-pendientes-by-usuario');
+  // FARMACIA
+  if (authorities.includes(INN_AUTHORITIES.FARMACIA.CODE)) response.push('inn-farmacia');
+  if (authorities.includes(INN_AUTHORITIES.FARMACIA.REPORTE_INVENTARIO)) response.push('inn-farmacia-reporte-inventario');
   return response;
 }
